ced2021b66abb458bce9fb0d3d6c9681c65fc4fd
Troy <hello@troylusty.com>
2024-02-16 15:57:53 +0000

    Mess with error catching

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,25 @@
 // Read file contents and hash with given password
 // Return hashed data out to new file
 
-use anyhow::{Context, Result};
+use anyhow::Result;
 use clap::Parser;
 use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
+use std::io::{BufReader, prelude::*};
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
+#[arg(short, long)]
+    path_to_file: std::path::PathBuf,
     #[arg(short, long)]
-    key: String,
-    #[arg(short, long)]
-    file_path: std::path::PathBuf,
+    key: String, 
 }
 
-fn write_file(file_path: &mut PathBuf, contents: String) -> std::io::Result<()> {
-    file_path.set_extension("enc");
-    let mut file = File::create(file_path)?;
-    write!(file, "{}", contents).expect("could not write to file");
+fn write_file(path: &mut PathBuf, contents: &String) -> Result<()> {
+    path.set_extension("enc");
+    let mut file = File::create(path).expect("could not create file");
+    write!(file, "{:?}", contents).expect("could not write to file");
     Ok(())
 }
 
@@ -36,19 +35,8 @@ fn read_file(file_path: &mut PathBuf) -> String {
 fn main() -> Result<()> {
     let mut args = Cli::parse();
 
-    println!("key: {:?}, file path: {:?}", &args.key, &args.file_path);
-
-    let _filevar = read_file(&mut args.file_path);
-    let _fileout = write_file(&mut args.file_path, _filevar);
-
-    let content = std::fs::read_to_string(&args.file_path)
-        .with_context(|| format!("could not read file `{}`", args.file_path.display()))?;
-
-    for line in content.lines() {
-        if line.contains(&args.key) {
-            println!("line: {}", line);
-        }
-    }
+    let _filevar = read_file(&mut args.path_to_file);
+    let _fileout = write_file(&mut args.path_to_file, &_filevar);
 
     Ok(())
 }
